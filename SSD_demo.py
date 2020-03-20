c185e9b4ee701e03c77dfe0211e9dc4945f4b0b2
pku-rsq <57979612+pku-rsq@users.noreply.github.com>
2020-03-20 13:50:05 +0800

    first commit

---
+++ SSD_demo.py
@@ -0,0 +1,37 @@
+"""
+SSD demo
+"""
+
+import cv2
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+from ssd_300_vgg import SSD
+from utils import preprocess_image, process_bboxes
+from visualization import plt_bboxes
+
+
+ssd_net = SSD()
+classes, scores, bboxes = ssd_net.detections()
+images = ssd_net.images()
+
+sess = tf.Session()
+# Restore SSD model.
+ckpt_filename = './ssd_checkpoints/ssd_vgg_300_weights.ckpt'
+sess.run(tf.global_variables_initializer())
+saver = tf.train.Saver()
+saver.restore(sess, ckpt_filename)
+
+img = cv2.imread('./demo/car_detection.jpg')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img_prepocessed = preprocess_image(img)
+rclasses, rscores, rbboxes = sess.run([classes, scores, bboxes],
+                                      feed_dict={images: img_prepocessed})
+rclasses, rscores, rbboxes = process_bboxes(rclasses, rscores, rbboxes)
+plt_title('This is title')
+plt_bboxes(img, rclasses, rscores, rbboxes)
+
